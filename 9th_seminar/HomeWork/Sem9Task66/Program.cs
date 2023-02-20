c3908d0efa801d71df5991c61dad5cabb56990ad
global user <my@example.com>
2023-02-21 01:06:53 +0300

    HomeWrok of 9th seminar was done

---
+++ 9th_seminar/HomeWork/Sem9Task66/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+//int m = 1;
+//int n = 15;
+//Ввод
+int m = ReadData("введите 1ое число: ");
+int n = ReadData("введите 2ое число: ");
+
+//Вывод
+int sum = (m < n)? (SumOfIntegers(m, n)):(SumOfIntegers(n, m));
+Console.WriteLine("Сумма позитивных чисел между " + m + " и " + n + " равна " + sum);
+
+
+///=============================Методы========================================
+//-М- метода генерации 2умерного случайного массива
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//-М- метода рекррентного подсчета суммы элементов (края включены в расчет) 
+int SumOfIntegers(int m, int n)
+{
+    if (m > n)
+    {
+        return 0;
+    }
+    else if (m <= 0)
+    {
+        return SumOfIntegers(1, n);
+    }
+    else
+    { return m + SumOfIntegers(m + 1, n); }
+}
---
+++ 9th_seminar/HomeWork/Sem9Task68/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+//Ввод
+int m = ReadData("введите 1ое число: ");
+int n = ReadData("введите 2ое число: ");
+
+//Вывод
+int result = Ackermann(m, n);
+Console.WriteLine(result);
+
+///=============================Методы========================================
+//-М- метода генерации 2умерного случайного массива
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//-М- функция Аккермана (по базовому определению)
+int Ackermann(int n, int m)
+{
+    if (n == 0)
+    { return m + 1; }
+    else if (m == 0)
+    { return Ackermann(n - 1, 1); }
+    else
+    { return Ackermann(n - 1, Ackermann(n, m - 1)); }
+}
